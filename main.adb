0c5078390155c4a04077d2a8e2ad89a37ee6e50a
benitol87 <benoit.leroy@etu.unilim.fr>
2016-01-12 14:33:51 +0100

    Ajout d'un fichier de configuration
    Plus besoin de compiler le programme pour modifier le nombre de rotations ou le nombre de points par courbe de bézier

--- main.adb
+++ main.adb
@@ -1,26 +1,77 @@
 with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Fixed,Ada.Strings; use Ada.Strings.Fixed,Ada.Strings;
+with Fichier; use Fichier;
+with Math; use Math;
 with STL; use STL;
 with Parser_Svg; use Parser_Svg;
-with Ada.Text_IO; use Ada.Text_IO;
-with Math; use Math;
 
 procedure Main is
-	Segments : Liste_Points.Liste;
-	Facettes : Liste_Facettes.Liste;
+    Segments : Liste_Points.Liste;
+    Facettes : Liste_Facettes.Liste;
+    FICHIER_CONFIG: String := "config.txt";
+
+    procedure Charger_Config is
+        Fic: File_Type;
+
+        procedure Config(Cle,Valeur: String) is
+            Val: Integer;
+        begin
+            if Cle = "nbRotations" then
+                Val := Integer'Value(Valeur);
+
+                if Val >2 then
+                    NB_ROTATIONS:=Val; -- On modifie la valeur si celle passée en config vaut au moins 3
+                end if;
+            elsif Cle = "nbPointsBezier" then
+                Val := Integer'Value(Valeur);
+
+                if Val > 1 then
+                    NB_POINTS_BEZIER := Val; -- On modifie la valeur si celle passée en config vaut au moins 2
+                end if;
+            else
+                Put_Line("Erreur lors de la lecture du fichier de configuration, clé non reconnue : " & Cle);
+            end if;
+        end;
+    begin
+        Ouvrir_Fichier_Lecture(Fic,FICHIER_CONFIG);
+
+        while not End_Of_File (Fic) loop
+            declare
+                Ligne : String := Trim(Get_Line (Fic),Both);
+                Indice: Integer := Ligne'First;
+            begin
+                while Indice<Ligne'Last and then Ligne(Indice) /= '=' loop
+                    Indice:=Indice+1;
+                end loop;
+
+                if Indice /= Ligne'First and then Indice /= Ligne'Last then
+                    Config(
+                        Trim(Ligne(Ligne'First..Indice-1),Both), -- clé
+                        Trim(Ligne(Indice+1..Ligne'Last) ,Both)  -- valeur
+                        );
+                end if;
+            end;
+        end loop;
+
+        Fermer_Fichier(Fic);
+    end;
 begin
 
-	if Argument_Count /= 2 then
-		Put_Line(Standard_Error,
-		"usage : " & Command_Name &
-		" fichier_entree.svg fichier_sortie.stl");
-		Set_Exit_Status(Failure);
-		return;
-	end if;
+    if Argument_Count /= 2 then
+        Put_Line(Standard_Error,
+        "usage : " & Command_Name &
+        " fichier_entree.svg fichier_sortie.stl");
+        Set_Exit_Status(Failure);
+        return;
+    end if;
 
-	--on charge la courbe de bezier et la convertit en segments
-	Chargement_Bezier(Argument(1), Segments);
-	--on convertit en facettes par rotation
-	Creation(Segments, Facettes);
-	--on sauvegarde le modele obtenu
-	Sauvegarder(Argument(2), Facettes);
+    -- Chargement de la configuration
+    Charger_Config;
+    --on charge la courbe de bezier et la convertit en segments
+    Chargement_Bezier(Argument(1), Segments);
+    --on convertit en facettes par rotation
+    Creation(Segments, Facettes);
+    --on sauvegarde le modele obtenu
+    Sauvegarder(Argument(2), Facettes);
 end;
